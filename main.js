4eb64d6166bb52f3ebb1f64038a5aff6fa628a5c
IakovTaranenko <IakovTaranenko@users.noreply.github.com>
2021-02-16 14:26:56 -0800

    Base code added

---
+++ main.js
@@ -0,0 +1,42 @@
+const {Client} = require("discord.js");
+const ReadLine = require("readline");
+const Chalk = require("chalk");
+const { readlink } = require("fs");
+const { exception } = require("console");
+const Axios = requre("axios");
+
+const Client = new Client();
+
+const Console = ReadLine.createInterface({
+    input: process.stdin, 
+    output: process.stdout
+})
+
+Console.question("Token >> ", Token => {
+    Client.login(Token)
+
+    client.on("ready", () => {
+        console.log(`${Chalk.greenBright("[RUNNING]")} - Scanner has started running.`);
+    });
+
+    client.on("message", Message => {
+        if(Message.content.includes("discord.gift") || Message.content.includes("discordapp.com/gifts/")) {
+            console.log(`${Chalk.yellow("[FOUND]")} - Found a message containing discord.gift!      Server: ${Message.guild.name}, User: ${Message.author.username}, Message: ${Message}`);
+            const MatchCase = /(discord\.(gift)|discordapp\.com\/gift)\/.+[a-z]/
+            const GiftURL = MatchCase.exec(message.content)
+            const GiftCode = GiftURL[0].split("/")[1];
+
+            console.log(`${Chalk.orange("[ATTEMPT]")} - Attempting to redeem code.      Server: ${Message.guild.name}, User: ${Message.author.username}, Code: ${GiftCode}`);
+            Axios({
+                method: "POST",
+                url: `https://discordapp.com/api/v6/entitlements/gift-codes/${GiftCode}/redeem`,
+                headers: {
+                    'Authorization': Client.Token
+                }
+            })
+            .then(() => console.log(`${Chalk.green("[SUCESS]")} - Reedemed nitro code.      Server: ${Message.guild.name}, User: ${Message.author.username}, Code: ${GiftCode}`))
+            .catch(Xxception => console.log(`${Chalk.red("[FAILED]")} - Code was invalid.       Server: ${Message.guild.name}, User: ${Message.author.username}, Code: ${GiftCode}`))
+        }
+    });
+
+})
